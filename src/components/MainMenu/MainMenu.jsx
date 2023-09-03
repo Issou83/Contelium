8439a833a79f590d1d369b337389e4205597376d
Issou83 <issou8@gmail.com>
2023-09-03 16:40:16 +0200

    Maj

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -5,7 +5,18 @@ import "./index.css"
 const MainMenu = ({ setView }) => {
   return (
     <div className='homeMenu'>
-            <h1 className='titleSplash'>CONSTELIUM</h1>
+            <h1 className='titleSplash'>
+              <span data-letter="C">C</span>
+              <span data-letter="O">O</span>
+              <span data-letter="N">N</span>
+              <span data-letter="S">S</span>
+              <span data-letter="T">T</span>
+              <span data-letter="E">E</span>
+              <span data-letter="L">L</span>
+              <span data-letter="I">I</span>
+              <span data-letter="U">U</span>
+              <span data-letter="M">M</span>
+            </h1>
 
       <button className='btnHomeMenu' onClick={() => setView('register')}>Créer un compte</button>
       <button className='btnHomeMenu' onClick={() => setView('login')}>Se connecter</button>
